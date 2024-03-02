ea3e60cada33f2d8c8bf0042099de31730693efa
RakSridhar23 <rss9243@nyu.edu>
2024-03-02 15:21:59 -0500

    search by title

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 from dotenv import find_dotenv, load_dotenv
 import os
 from werkzeug.utils import secure_filename  # allowing file uploads
@@ -120,8 +120,16 @@ def register():
 
 @app.route("/search")
 def search():
-    internships = internships_collection.find()
-    internship_list = list(internships)
+    query = request.args.get("query", "")
+    
+    if query:
+        internships = internships_collection.find({"title": {"$regex": query, "$options": "i"}})
+        
+    else:
+        
+        internships = internships_collection.find()
+         
+        internship_list = list(internships)
     return render_template("search.html", internships=internship_list)
 
 
