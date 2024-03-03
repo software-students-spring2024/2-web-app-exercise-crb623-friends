d01e4f8a77f88fb75431a23d7dc93b617a286e8d
denprud <denzelprud1@gmail.com>
2024-03-03 11:51:14 -0500

    Removed mock db functionality and added comments

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ from werkzeug.utils import secure_filename  # allowing file uploads
 from werkzeug.security import generate_password_hash, check_password_hash
 import flask_login
 
-
-
-
 "i want to get environment variables from my .env"
-
 
 load_dotenv()
 
@@ -22,6 +18,7 @@ DB_HOST = os.getenv("DB_HOST")
 UPLOAD_FOLDER = "resumes"
 ALLOWED_EXTENSIONS = {"pdf", "doc", "docx"}
 
+# Database Initialization
 uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true"
 client = MongoClient(uri)
 db = client.get_database("internships")
@@ -38,12 +35,10 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+
+# Initialize the login manager
 login_manager = flask_login.LoginManager()
-
 login_manager.init_app(app)
-
-# Mock Login Data.
-users = {"foo@bar.tld": {"password": "secret"}}
 
 # Mock user profile data - will be deleted once MongoDB database is active
 user_profile = {
@@ -63,28 +58,30 @@ user_profile = {
 class User(flask_login.UserMixin):
     pass
 
-
+# Loads user from current session
 @login_manager.user_loader
 def user_loader(email):
-    if email not in users:
+    results = internships_collection.find_one({'email' : email})
+    if not results:
         return
 
     user = User()
     user.id = email
     return user
 
-
+# Loads user from flask request
 @login_manager.request_loader
 def request_loader(request):
     email = request.form.get("email")
-    if email not in users:
+    results = internships_collection.find_one({'email' : email})
+    if not results:
         return
 
     user = User()
     user.id = email
     return user
 
-
+# The route for the log-in page
 @app.route("/", methods=["GET", "POST"])
 def login():
     error = None
@@ -93,13 +90,15 @@ def login():
         password = request.form['password']
         action = request.form['action']
         
+        # Attempts to find a DB entry from the Mongo Database
         results = internships_collection.find_one({'email' : email})
 
+        # Checks to see if user is already is in the databse
         if action == 'signin' and results and check_password_hash(results['password'], password):
             user = User()
             user.id = email
             flask_login.login_user(user)
-            # Redirect to the home page upon successful login - need database interaction added
+            # Redirect to the home page upon successful login
             return redirect(url_for("search"))
         elif action == "signup":
             return redirect(url_for("register"))
@@ -115,6 +114,7 @@ def register():
         email = request.form['email']
         password = request.form['password']
         existing_user = internships_collection.find_one({'email' : email})
+        # Adds a Database entry if the user isn't already registered
         if existing_user is None:
             hash_pass = generate_password_hash(password)
             internships_collection.insert_one({'email' : email, 'password' : hash_pass, 'name' : name})
